9dc7da6f9d33019388586d17ceb7b7b0e9df7325
T3r1jj <t3r1jj@gmail.com>
2017-10-16 10:32:40 +0200

    Refactored and created simple test

---
+++ src/test/java/io/gitlab/swded/system/model/ParserTest.java
@@ -0,0 +1,21 @@
+package io.gitlab.swded.system.model;
+
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ParserTest {
+
+    @Test
+    public void parse() {
+        String dataSetName = "IRISDAT.TXT";
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(getClass().getClassLoader().getResourceAsStream(dataSetName)))) {
+            Parser parser = new Parser(bufferedReader);
+            parser.parse();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
